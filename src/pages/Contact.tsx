d3f4f1a4af1e89d0ca9ebb47890335f546408e85
Kesava masanam <masanamkesava@gmail.com>
2025-09-23 04:03:47 +0530

    your message about the changes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -221,6 +221,7 @@ const Contact = () => {
                     {/* Hidden FormSubmit options */}
                     <input type="hidden" name="_template" value="table" />
                     <input type="hidden" name="_captcha" value="false" />
+                    <input type="hidden" name="_next" value="https://smartportfolio-ai.netlify.app/contact"></input>
 
                     <Button
                       type="submit"
